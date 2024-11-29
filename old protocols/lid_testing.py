c8b207ba0c35823fa562a2b908dfdc69a5f0f636
unknown <nico.luu.to@gmail.com>
2024-11-29 12:27:13 -0500

    OMG THE GRIPPER CAN MOVE THE LID YAYY

--- old protocols/lid_testing.py
+++ old protocols/lid_testing.py
@@ -11,4 +11,6 @@ requirements = {"robotType": "Flex", "apiLevel": "2.19"}
 def run(protocol: protocol_api.ProtocolContext):
     sample_plate = protocol.load_labware("corning_96_wellplate_360ul_flat", "C2")
     lid = protocol.load_labware("corning_96_wellplate_360ul_flat", location= "C1")
+    lid.set_offset(x=0.00, y=0.00, z=30)
+    print(protocol.deck.__delitem__('C2'))
     protocol.move_labware(labware=lid, new_location="C2", use_gripper=True)
